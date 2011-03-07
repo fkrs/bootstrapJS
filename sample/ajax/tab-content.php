e0472dc5a034800f478cf9ba408051e320dec60c
slyg(work) <syl.faucherand@gmail.com>
2011-03-07 19:28:09 +0100

    adding ajax delay

--- sample/ajax/tab-content.php
+++ sample/ajax/tab-content.php
@@ -1,5 +1,6 @@
 <?php
 header("Content-type: text/html");
+sleep(1);
 ?>
 
 <p>Cras id augue nec enim laoreet aliquam vitae eu augue. Sed elementum lacus non nibh elementum vitae rutrum nibh faucibus. Nunc sed tellus rutrum lorem volutpat rhoncus vel a dolor. Nunc est enim, ullamcorper hendrerit tincidunt nec, faucibus non nisi.</p>
